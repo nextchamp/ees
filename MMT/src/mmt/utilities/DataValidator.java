77f25f466b4c5664c0bc8281fd56cf1d82f187d1
Praveen.Kumar <Praveen.Kumar@US544249996DEB.am.sony.com>
2013-01-31 02:09:24 -0800

    Fixed bugs: 1) Field placement was not updating 2) Restore/Backup was not funtional fully/buggy 3) Update of detais pane and title 4) ChoiceBox to ComboBox 5) Print on multiple page enabled 6) More try/catch

--- MMT/src/mmt/utilities/DataValidator.java
+++ MMT/src/mmt/utilities/DataValidator.java
@@ -4,6 +4,8 @@
  */
 package mmt.utilities;
 
+import mmt.CalendarUtil;
+
 /**
  *
  * @author Praveen.Kumar
@@ -11,17 +13,83 @@ package mmt.utilities;
 public class DataValidator {
     
     /*
-     * Method to validate the zip code
+     * Returns true if name is not empty/null else false
      * 
     */
-    public static boolean isZipCodeValid(String s) {
+    public static boolean isNameValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        }
+        
+        return true;
+    }
+    /*
+     * Returns true if street is not empty/null else false
+     * 
+    */
+    public static boolean isStreetValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        }
+        
+        return true;
+    }
+    /*
+     * Returns true if city is not empty/null else false
+     * 
+    */
+    public static boolean isCityValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        }
+        
+        return true;
+    }
+    /*
+     * Returns true if state is not empty/null else false
+     * 
+    */
+    public static boolean isStateValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        }
+        
         return true;
     }
     /*
      * Method to validate the zip code
      * 
     */
+    public static boolean isZipCodeValid(String s) {
+        
+        if (s == null || s.isEmpty()) {
+          return false;
+        } else {
+            // try to parse the postal code into an int
+            try {
+                s = s.trim();
+                Integer.parseInt(s);
+                if (s.length() == 5) {
+                    return true;
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+        return false;
+    }
+    /*
+     * Method to validate the zip code
+     * 
+    */
     public static boolean isEmailValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        } else if (s.indexOf("@") == -1) {
+            return false;
+        }else if (s.indexOf(".") == -1) {
+            return false;
+        }
+        
         return true;
     }
     /*
@@ -29,6 +97,24 @@ public class DataValidator {
      * 
     */
     public static boolean isPhoneNumberValid(String s) {
+        if (s == null || s.isEmpty()) {
+          return false;
+        }
+            
+        return true;
+    }
+    /*
+     * Method to validate the date
+     * 
+    */
+    public static boolean isDateValid(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        } else {
+            if (!CalendarUtil.validString(s)) {
+                return false; // "No valid birthday. Use the format yyyy-mm-dd!\n";
+            }
+        }
         return true;
     }
     public DataValidator() {
